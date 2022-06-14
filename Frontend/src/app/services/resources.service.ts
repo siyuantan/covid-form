88133902ed9303967db6550b59fd834124e9dfdc
Siyuan Tan <tsy2009@hotmail.com>
2022-06-14 23:38:41 +0800

    Updated end points addresses on frontend caller

--- Frontend/src/app/services/resources.service.ts
+++ Frontend/src/app/services/resources.service.ts
@@ -11,7 +11,7 @@ export class ResourcesService {
   constructor(private http: HttpClient) {}
   
   getAllCovidDeclarations(): Observable<CovidDeclaration[]> {
-    return this.http.get<CovidDeclaration[]>('http://localhost:8080/api/covidDeclarations')
+    return this.http.get<CovidDeclaration[]>('http://localhost:8080/api/covid-declarations')
     .pipe(map(covidDeclarations => covidDeclarations.map(declaration => CovidDeclaration.toBusinessEntity(declaration))));
   }
 }
